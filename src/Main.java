0426310dbfd1578994bce3a8d02f965982b3fe82
Caio Rufino <caiofurlanrufinosilva@gmail.com>
2024-07-24 10:32:18 -0300

    Curso e mentoria

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("Curso JavaScript");
+        curso1.setDescricao("Descrição curso JavaScript");
+        curso1.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
